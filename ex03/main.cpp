e7220b03153ec21affd2cf34e310577df0d51073
yonuma28 <kisaragi.12056@gmail.com>
2025-12-24 10:37:08 +0900

    fix: 整形

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -26,7 +26,7 @@ int main()
   {
     Weapon club = Weapon("crude spiked club");
     HumanB jim("Jim");
-	jim.attack();
+	  jim.attack();
     jim.setWeapon(club);
     jim.attack();
     club.setType("some other type of club");

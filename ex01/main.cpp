8da77606e03af108f0d087254ceb9eab3dc66dc0
yukito-onuma <onuma.y.473@ms.saitama-u.ac.jp>
2025-12-28 19:54:54 +0900

    fix: try-catch 文を追加

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,16 +1,22 @@
 #include "Zombie.hpp"
 
 int main() {
-	int N = 2147483647;
-	Zombie* horde = zombieHorde(N, "Zombie");
-	if (horde) {
-		for (int i = 0; i < N; ++i) {
-			horde[i].announce();
-		}
-		delete[] horde;
-	} else {
-		std::cerr << "Failed to create zombie horde." << std::endl;
-		return 1;
-	}
-	return 0;
+    int N = 100000;
+    Zombie* horde = NULL;
+
+    try {
+        horde = zombieHorde(N, "Zombie");
+        if (horde) {
+            for (int i = 0; i < N; ++i) {
+                horde[i].announce();
+            }
+            delete[] horde;
+        }
+		else
+			return (1);
+    } catch (const std::exception& e) {
+        std::cerr << "Fatal Error: " << e.what() << std::endl;
+        return 1;
+    }
+    return 0;
 }
--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -3,20 +3,14 @@
 
 Zombie* zombieHorde(int N, std::string name)
 {
-    if (N <= 0) { 
+    if (N <= 0 || 10000 < N) { 
         std::cerr << "Error: Invalid number of zombies (N=" << N << ")." << std::endl;
         return NULL;
     }
 
-    Zombie* horde = NULL;
-    try {
-        horde = new Zombie[N];
-        for (int i = 0; i < N; i++) {
-            horde[i].setName(name);
-        }
-    } catch (const std::exception& e) {
-        std::cerr << "Error: Allocation failed: " << e.what() << std::endl;
-        return NULL;
+    Zombie* horde = new Zombie[N];
+    for (int i = 0; i < N; i++) {
+        horde[i].setName(name);
     }
     return horde;
 }

51dd2ca3a8636e2423919fee2cc99e86554d612d
yukito-onuma <onuma.y.473@ms.saitama-u.ac.jp>
2025-12-28 19:37:19 +0900

    fix: try-catch 文を追加

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,11 +1,18 @@
 #include "Zombie.hpp"
 
 int main() {
-	Zombie *Zombie1;
-
-	Zombie1 = newZombie("Zombie1");
-	Zombie1->announce();
-	delete Zombie1;
-
+	try
+	{
+		Zombie* Zombie1 = newZombie("Zombie1");
+		Zombie1->announce();
+		delete Zombie1;
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
+		return (1);
+	}
 	randomChump("Zombie2");
+
+	return (0);
 }
--- ex00/newZombie.cpp
+++ ex00/newZombie.cpp
@@ -2,14 +2,5 @@
 
 Zombie* newZombie(std::string name_)
 {
-    try {
-        Zombie* zombie_ptr = new Zombie(name_);
-        return zombie_ptr;
-    } catch (const std::bad_alloc& e) {
-        std::cerr << "Memory allocation failed: " << e.what() << std::endl;
-        return nullptr;
-    } catch (...) {
-        std::cerr << "Unknown exception occurred during Zombie creation." << std::endl;
-        throw;
-    }
+    return new Zombie(name_);
 }
